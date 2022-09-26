d7a9ad7a776299e59994f24f535bf612026a81df
Jose I Toledano Morales <jose.toledano@acigroup.com.mx>
2022-09-26 11:31:49 -0500

    Fix name funtion

--- app.py
+++ app.py
@@ -9,16 +9,16 @@ def hello():
     return "This is my test ABRAXAS!!!"
 
 @app.route('/getcounter')
-def index():
+def getcounter():
     out = counter.value
     return jsonify(count=out)
 
 @app.route("/postrequest", methods=["POST"])
-def index():
+def postrequest():
     with counter.get_lock():
         counter.value += 1
         out = counter.value
-    return jsonify(count=out)
+    return {"status":"Ok"}
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
