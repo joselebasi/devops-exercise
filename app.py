1557a04d8319941ca23554e904d792b43e28e6ca
Jose I Toledano Morales <jose.toledano@acigroup.com.mx>
2022-09-26 10:10:34 -0500

    Counter post request service

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask,jsonify
+from multiprocessing import Value
 
+counter = Value('i', 0)
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "This is my test ABRAXAS!!!"
 
+@app.route('/getcounter')
+def index():
+    out = counter.value
+    return jsonify(count=out)
+
+@app.route("/postrequest", methods=["POST"])
+def index():
+    with counter.get_lock():
+        counter.value += 1
+        out = counter.value
+    return jsonify(count=out)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
