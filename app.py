c5d3987a92b39232d5b2f6096feeabeb2fee153d
Jose I Toledano Morales <jose.toledano@acigroup.com.mx>
2022-09-28 09:33:59 -0500

    New service redis

--- app.py
+++ app.py
@@ -7,25 +7,25 @@ from multiprocessing import Value
 
 #redis = Redis(host=os.getenv('REDIS_HOST', 'redis-service'),
 #              port=os.getenv('REDIS_PORT', 30001))
-redis = Redis(host=os.getenv('REDIS_HOST', 'redis-service'),
-              port=os.getenv('REDIS_PORT', 30001))
+redis = Redis(host=os.getenv('REDIS_HOST', 'redis-master'),
+              port=os.getenv('REDIS_PORT', 6379))
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return {"path":"root","version":"1.4"}
+    return {"path":"root","version":"1.6"}
 
 @app.route('/getcounter',methods = ['POST', 'GET'])
 def getcounter():
     if request.method == 'POST':
         redis.incr('counter')
-        return {"status":"ok"}
+        return {"method":"POST","status":"ok"}
     else:
         try:
             hits = int(redis.get('counter'))
         except:
-            return jsonify(count=0)   
-    return jsonify(count=hits)
+            return {"method":"GET","count":"0"}  
+    return {"method":"GET","count":hits}
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
