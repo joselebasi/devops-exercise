0f63be7ea8a833b12306db1ef55155777fd82043
Jose I Toledano Morales <jose.toledano@acigroup.com.mx>
2022-10-06 14:59:43 -0500

    Create configmap and secrets

--- app.py
+++ app.py
@@ -9,26 +9,29 @@ from multiprocessing import Value
 #              port=os.getenv('REDIS_PORT', 30001))
 redis = Redis(host=os.getenv('REDIS_HOST', 'redis'),
               port=os.getenv('REDIS_PORT', 6379),
-              password='a-very-complex-password-here')
+              password=os.getenv('REDIS_PASS', 'a-very-complex-password-here'),
+              )
+
 app = Flask(__name__)
 pod_name=os.getenv('MY_POD_NAME', 'pod-01')
+developer=os.getenv('NAME_DEV', 'default value')
 
 @app.route("/")
 def hello():
-    return {"path":"/","version":"1.8","pod-name":pod_name}
+    return {"path":"/","version":"1.8","pod-name":pod_name,"name-dev":developer}
 
 @app.route('/getcounter',methods = ['POST', 'GET'])
 def getcounter():
     
     if request.method == 'POST':
         redis.incr('counter')        
-        return {"method":"POST","status":"ok","pod-name":pod_name}
+        return {"method":"POST","status":"ok","pod-name":pod_name,"name-dev":developer}
     else:
         try:
             hits = int(redis.get('counter'))
         except:
-            return {"method":"GET","count":"0","pod-name":pod_name}  
-    return {"method":"GET","count":hits,"pod-name":pod_name}
+            return {"method":"GET","count":"0","pod-name":pod_name,"name-dev":developer}  
+    return {"method":"GET","count":hits,"pod-name":pod_name,"name-dev":developer}
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
