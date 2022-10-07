d9d6919afc5a58bab16318e23c905d1fa0f11a87
Jose I Toledano Morales <jose.toledano@acigroup.com.mx>
2022-10-07 16:09:28 -0500

    Add service restart 4

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ evport = os.getenv('REDIS_PORT', 6379)
 pod_name=os.getenv('MY_POD_NAME', 'pod-01')
 developer=os.getenv('NAME_DEV', 'default value')
 evpass = os.getenv('REDIS_PASS', 'a-very-complex-password-here')
-redis = Redis(host=evhost,
-              port=int(evport),
-              password=evpass)
+redis = Redis(host='redis',
+              port=6379,
+              password='a-very-complex-password-here')
 
 @app.route("/")
 def hello():
@@ -27,7 +27,7 @@ def hello():
     print("port:"+evport)
     print("pass:"+evpass)
     print("--------------")
-    return {"path":"/","version":"1.13","pod-name":pod_name,"name-dev":developer}
+    return {"path":"/","version":"1.15","pod-name":pod_name,"name-dev":developer}
 
 @app.route('/getcounter',methods = ['POST', 'GET'])
 def getcounter():    
@@ -38,13 +38,14 @@ def getcounter():
         try:
             hits = int(redis.get('counter'))
         except:
-            return {"method":"GET","count":"0","pod-name":pod_name,"name-dev":developer}  
+            return {"method":"GET","error":"Connection to redis does not resolve","pod-name":pod_name,"name-dev":developer}  
     return {"method":"GET","count":hits,"pod-name":pod_name,"name-dev":developer}
 
 
 @app.route('/restart',methods = ['POST'])
 def setrestart():
     redis.set('counter',0)
+    print("COUNTER RESTARTED")
     
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
